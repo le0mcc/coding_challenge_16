e1cd1cc222cec637e58894a4456fecbfc502beb0
Leo McClellan <mcclellan2@usf.edu>
2024-11-17 14:43:15 -0500

    Set up App component with initial product list

--- my_components/ProductList.jsx
+++ my_components/ProductList.jsx
@@ -1,9 +1,11 @@
 import ProductItem from "./ProductItem";
-
+// Create ProductList Component
 function ProductList({productList}) {
+    // Creating an Array of Product Items to be Rendered
     const listOfProducts = productList.map(product =>
         <ProductItem product={product} key={product.id}/>
     );
+    // Reder Product Items
     return (
     <>
         <ul>{listOfProducts}</ul>
